1c204dfd00542d68221f3fd9fb1b4ddf0bc006c7
Nick Whitehill <nicholas.whitehill@gmail.com>
2016-10-01 15:42:09 -0700

    Created basic Beans. Defined Server -> mongo interface

---
+++ Backend/src/main/java/beans/Team.java
@@ -0,0 +1,7 @@
+package beans;
+
+/**
+ * Created by nick on 10/1/16.
+ */
+public class Team {
+}
---
+++ Backend/src/main/java/mongohandler/MongoOperator.java
@@ -0,0 +1,25 @@
+package mongohandler;
+
+import beans.Profile;
+import beans.Team;
+import util.AccountID;
+
+import java.util.List;
+
+/**
+ * Created by nick on 10/1/16.
+ */
+public interface MongoOperator {
+
+    boolean exists (AccountID account);
+
+    void populateProfile(Profile profile);
+
+    void populateTeam(Team team);
+
+    void getProfilesWithIDs(List<AccountID> accountIDs);
+
+    void getProfiles(List<Profile> profiles);
+
+    void addTeamMember(Team team, Profile newTeamMember);
+}
