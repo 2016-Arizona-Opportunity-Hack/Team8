3c82d2dcd463fb5be991c09e4d54932310fd435c
Harry Chang <harryhaw94@gmail.com>
2016-10-02 04:23:22 -0700

    twitter-config

---
+++ twitter-config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+@author muni
+@copyright http:www.smarttutorials.net
+ */
+
+require_once 'messages.php';
+
+//site specific configuration declartion
+define( 'BASE_PATH', 'http://demo.smarttutorials.net/twitter/');
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USERNAME', 'root');
+define( 'DB_PASSWORD', '');
+define( 'DB_NAME', 'user_login');
+
+//Twitter login
+define('TWITTER_CONSUMER_KEY', 'JqXTytyqpfEd72RiqzgCCoXVK');
+define('TWITTER_CONSUMER_SECRET', 'f4RQNkB3h0YofvgCsiT6Fs8cEesdxyFQ7oi1WHYo3JmEh6leAf');
+define('TWITTER_OAUTH_CALLBACK', 'http://localhost8080');
+
+function __autoload($class)
+{
+	$parts = explode('_', $class);
+	$path = implode(DIRECTORY_SEPARATOR,$parts);
+	require_once $path . '.php';
+}
