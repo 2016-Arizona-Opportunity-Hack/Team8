84966ff150c2e531e78d3a8d31115611ba69f8f1
Nick Whitehill <nicholas.whitehill@gmail.com>
2016-10-01 22:39:00 -0700

    Adding more unit tests to MongoHandler.

--- Backend/src/test/java/mongohandler/MongoHandlerTest.java
+++ Backend/src/test/java/mongohandler/MongoHandlerTest.java
@@ -1,7 +1,31 @@
 package mongohandler;
 
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+
 /**
  * Created by nick on 10/1/16.
  */
-public class MongoHandlerTest {
+public class MongoHandlerTest{
+
+    @Test
+    public void testDBConnection() throws Exception {
+        MongoClient client = new MongoClient();
+//        MongoDatabase db = client.getDatabase(MongoConstants.DATABASE_NAME);
+//        db.createCollection(MongoConstants.PROFILE_COLLECTION_NAME);
+        Iterable<String> databases = client.listDatabaseNames();
+        Iterator dbIterator = databases.iterator();
+        while(dbIterator.hasNext()){
+            System.out.println(dbIterator.next());
+        }
+    }
+
+    @Test
+    public void testProfileExists() throws Exception {
+
+    }
 }
