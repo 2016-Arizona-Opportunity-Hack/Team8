9ad2256b00440b0178d9d343f4522593c145c6de
Nick Whitehill <nicholas.whitehill@gmail.com>
2016-10-01 23:01:32 -0700

    Updating testCases

--- Backend/src/test/java/mongohandler/MongoHandlerTest.java
+++ Backend/src/test/java/mongohandler/MongoHandlerTest.java
@@ -1,8 +1,14 @@
 package mongohandler;
 
 
+import beans.AccountID;
+import beans.Profile;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Iterator;
@@ -12,11 +18,27 @@ import java.util.Iterator;
  */
 public class MongoHandlerTest{
 
+    MongoClient client;
+    MongoDatabase mongoDatabase;
+
+    @BeforeClass
+    public void setup() throws Exception {
+        client = new MongoClient();
+        mongoDatabase = client.getDatabase(MongoConstants.DATABASE_NAME);
+    }
+
+    public Document getTestDocument(){
+        Document testProfile = new Document(MongoConstants.FIRST_NAME_FIELD, "TEST")
+                .append(MongoConstants.LAST_NAME_FIELD, "PROFILE")
+                .append(MongoConstants.TEAM_ID_FIELD, "")
+                .append(MongoConstants.TOTAL_MILES_FIELD, 100)
+                .append(MongoConstants.ACCOUNT_ID_FIELD, "test@test.com");
+        return testProfile;
+    }
+
     @Test
     public void testDBConnection() throws Exception {
         MongoClient client = new MongoClient();
-//        MongoDatabase db = client.getDatabase(MongoConstants.DATABASE_NAME);
-//        db.createCollection(MongoConstants.PROFILE_COLLECTION_NAME);
         Iterable<String> databases = client.listDatabaseNames();
         Iterator dbIterator = databases.iterator();
         while(dbIterator.hasNext()){
@@ -25,7 +47,28 @@ public class MongoHandlerTest{
     }
 
     @Test
-    public void testProfileExists() throws Exception {
+    public void profileExistsTest() throws Exception {
+        MongoCollection<Document> profileCollection = mongoDatabase.getCollection(MongoConstants.PROFILE_COLLECTION_NAME);
+
+        Document testProfile = getTestDocument();
+        profileCollection.insertOne(testProfile);
+
+        MongoHandler handler = new MongoHandler();
+
+        AccountID testID = new AccountID();
+        testID.setAccountID("test@test.com");
+        Assert.assertTrue(handler.exists(testID), "The test ID does exist in the DB");
+
+        testID.setAccountID("negative@test.com");
+        Assert.assertFalse(handler.exists(testID), "The test ID Does not exist in DB");
 
     }
+
+    @Test
+    public void populateProfileTest() throws Exception {
+        Profile testProfile = new Profile();
+        testProfile.setTeamName("TEST_TEAM");
+    }
+
+
 }
