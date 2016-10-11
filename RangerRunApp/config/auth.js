7822eece0b0bd5785aa28dd09da3eca5b87f3c01
monkeydunkey <bhushan.shashank93@gmail.com>
2016-10-10 22:33:14 -0700

    file clean up

--- RangerRunApp/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'facebookAuth': {
-      'clientID'      : '1221863707852050', // your App ID
-      'clientSecret'  : '7e87efcd3e5b590d275fcfcb276ec253', // your App Secret
-      'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-  }
-}
