b787726eef38df23753b69baa95012b1f7aebce8
monkeydunkey <bhushan.shashank93@gmail.com>
2016-10-10 23:12:20 -0700

    Revert "file clean up"
    This reverts commit 7822eece0b0bd5785aa28dd09da3eca5b87f3c01.

---
+++ RangerRunApp/config/passport.js
@@ -0,0 +1,54 @@
+// config/passport.js
+
+// load all the things we need
+var LocalStrategy   = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var configAuth = require('./auth');
+var Users = require('../app/models/user');
+// expose this function to our app using module.exports
+module.exports = function(passport) {
+
+	// =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.rangerID);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+      // TODO: Implement searching algorithm to return correct user based on ID
+        user = Users.findById(id);
+        done(null, user);
+
+    });
+
+    // =========================================================================
+    // FACEBOOK LOGIN ==========================================================
+    // =========================================================================
+    passport.use('facebook', new FacebookStrategy({
+      clientID        : configAuth.facebookAuth.clientID,
+      clientSecret    : configAuth.facebookAuth.clientSecret,
+      callbackURL     : configAuth.facebookAuth.callbackURL,
+      profileFields: ['id', 'displayName', 'email']
+    },
+    function(token, refreshToken, profile, done){
+      process.nextTick(function(){
+        var user = {
+          'facebook'         : {
+              'id'           : profile.id,
+              'token'        : token,
+              'email'        : profile.emails[0].value,
+              'name'         : profile.displayName
+          },
+          'rangerID' : Math.random() * 1000
+        }
+        Users.save(user)
+        return done(null, user);
+      });
+    }))
+};
